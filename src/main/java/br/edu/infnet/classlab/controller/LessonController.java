aebb8b920b4df215b0c04cb41db4e8a48c33ebcb
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-05-18 20:23:54 -0300

    Ajustes nas descrições paa o swagger

--- src/main/java/br/edu/infnet/classlab/controller/LessonController.java
+++ src/main/java/br/edu/infnet/classlab/controller/LessonController.java
@@ -1,15 +1,16 @@
 package br.edu.infnet.classlab.controller;
 
 import br.edu.infnet.classlab.model.Lesson;
-import br.edu.infnet.classlab.model.Teacher;
 import br.edu.infnet.classlab.service.LessonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/lesson")
@@ -19,18 +20,33 @@ public class LessonController {
     private LessonService lessonService;
 
     @GetMapping
+    @Operation(summary = "Retorna todas as aulas", description = "Este endpoint retorna uma lista de todas as aulas disponíveis. Se não houver nenhuma aula, retornará 404 Not Found.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aulas retornadas com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Nenhuma aula encontrada")
+    })
     public ResponseEntity<List<Lesson>> getAllLessons() {
         List<Lesson> lessons = lessonService.getAllLessons();
         return lessons.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(lessons);
     }
 
     @PostMapping
+    @Operation(summary = "Cria uma nova aula", description = "Este endpoint cria uma nova aula com base nos dados fornecidos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Aula criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos para a criação da aula")
+    })
     public ResponseEntity<Lesson> createLesson(@RequestBody Lesson lesson) {
         Lesson newLesson = lessonService.saveLesson(lesson);
         return ResponseEntity.status(HttpStatus.CREATED).body(newLesson);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retorna uma aula por ID", description = "Este endpoint retorna uma aula específica com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aula retornada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Aula não encontrada")
+    })
     public ResponseEntity<Lesson> getLessonById(@PathVariable Long id) {
         return lessonService.getLessonById(id)
                 .map(ResponseEntity::ok)
@@ -38,18 +54,35 @@ public class LessonController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza uma aula existente", description = "Este endpoint atualiza uma aula específica com base no ID fornecido e nos novos dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aula atualizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Aula não encontrada"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos para a atualização")
+    })
     public ResponseEntity<Lesson> updateLesson(@PathVariable Long id, @RequestBody Lesson updatedLesson) {
         Lesson lesson = lessonService.updateLesson(id, updatedLesson);
         return ResponseEntity.ok(lesson);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Exclui uma aula por ID", description = "Este endpoint exclui uma aula específica com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Aula excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Aula não encontrada")
+    })
     public ResponseEntity<Lesson> deleteLessonById(@PathVariable Long id) {
         lessonService.deleteLessonById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/assign-teacher")
+    @Operation(summary = "Associa um professor a uma aula", description = "Este endpoint associa um professor específico a uma aula específica com base nos IDs fornecidos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Professor associado à aula com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Aula ou professor não encontrados"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos para a associação")
+    })
     public ResponseEntity<Lesson> assignTeacherToLesson(@RequestParam Long lessonId, @RequestParam Long teacherId) {
         Lesson assignedLesson = lessonService.assignTeacherToLesson(lessonId, teacherId);
         return ResponseEntity.ok(assignedLesson);
--- src/main/java/br/edu/infnet/classlab/controller/TeacherController.java
+++ src/main/java/br/edu/infnet/classlab/controller/TeacherController.java
@@ -2,6 +2,9 @@ package br.edu.infnet.classlab.controller;
 
 import br.edu.infnet.classlab.model.Teacher;
 import br.edu.infnet.classlab.service.TeacherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +20,22 @@ public class TeacherController {
     private TeacherService teacherService;
 
     @GetMapping
+    @Operation(summary = "Retorna todos os professores", description = "Este endpoint retorna uma lista de todos os professores disponíveis. Se não houver nenhum professor, retornará 404 Not Found.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Professores retornados com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Nenhum professor encontrado")
+    })
     public ResponseEntity<List<Teacher>> getAllTeachers() {
         List<Teacher> teachers = teacherService.getAllTeachers();
         return teachers.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(teachers);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retorna um professor por ID", description = "Este endpoint retorna um professor específico com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Professor retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Professor não encontrado")
+    })
     public ResponseEntity<Teacher> getTeacherById(@PathVariable Long id) {
         return teacherService.getTeacherById(id)
                 .map(ResponseEntity::ok)
@@ -30,18 +43,34 @@ public class TeacherController {
     }
 
     @PostMapping
+    @Operation(summary = "Cria um novo professor", description = "Este endpoint cria um novo professor com base nos dados fornecidos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Professor criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos para a criação do professor")
+    })
     public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher) {
         Teacher newTeacher = teacherService.saveTeacher(teacher);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTeacher);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza um professor existente", description = "Este endpoint atualiza um professor específico com base no ID fornecido e nos novos dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Professor atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Professor não encontrado"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos para a atualização")
+    })
     public ResponseEntity<Teacher> updateTeacher(@PathVariable Long id, @RequestBody Teacher teacher) {
         Teacher updatedTeacher = teacherService.updateTeacherById(id, teacher);
         return ResponseEntity.ok(updatedTeacher);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Exclui um professor por ID", description = "Este endpoint exclui um professor específico com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Professor excluído com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Professor não encontrado")
+    })
     public ResponseEntity<Teacher> deleteTeacherById(@PathVariable Long id) {
         teacherService.deleteTeacherById(id);
         return ResponseEntity.noContent().build();
